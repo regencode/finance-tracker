0f9cddb51b7151eb330332f99ec6d54106b3ddf4
regencode <thomgozalie@gmail.com>
2025-12-17 20:56:11 +0700

    feat

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [UserController],
   providers: [UserService, UserRepository],
 })
