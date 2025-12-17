2a5589ffa5f7d726c488ab2c9ab24fb633afe354
regencode <thomgozalie@gmail.com>
2025-12-17 21:05:03 +0700

    feat

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -7,6 +7,6 @@ import { PrismaModule } from 'src/prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserRepository, UserService],
 })
 export class UserModule {}
--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,11 +1,11 @@
 import { Injectable } from "@nestjs/common";
-import { PrismaClient } from "@prisma/client";
 import { CreateUserDto } from "./dto/create-user.dto";
+import { PrismaService } from "src/prisma/prisma.service";
 
 
 @Injectable()
 export class UserRepository {
-    constructor(private readonly prisma: PrismaClient) {}
+    constructor(private readonly prisma: PrismaService) {}
     async create(dto : CreateUserDto) {
         return await this.prisma.user.create({
             data: {
